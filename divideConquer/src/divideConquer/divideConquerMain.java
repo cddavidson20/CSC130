08288564789bbb4dec774b1e7c1c5faaa6430c3a
Chris Davidson <cddavidson@csus.edu>
2019-11-14 12:28:30 -0800

    created divide conquer algorithim

--- divideConquer/src/divideConquer/divideConquerMain.java
+++ divideConquer/src/divideConquer/divideConquerMain.java
@@ -3,31 +3,36 @@ package divideConquer;
 public class divideConquerMain {
     public static void main(String[] args) {
         System.out.println("Started");
-        int[] array1 = {-10,-18,1,2,3,4,5,5,7,8};
-        int[] array2 = {3,5,2,6,7,8,21,6,7};
+        int[] array1 = {2,3,4,5,7,35};
+        int[] array2 = {7,13,20,32};
+        //which nth largest element
+        int n = 8;
 
-        int arr1Largest = findLargest(array1); //find the largest in arr1
-        int arr2Largest = findLargest(array2); //find the largest in arr2
+        int theNth = findNth(array1, array2, array1.length, array2.length, n);
 
-        System.out.println("Max from array1: " + arr1Largest);
-        System.out.println("Max from array1: " + arr2Largest);
-
-        System.out.print("The largest is: " + largestVal(arr1Largest, arr2Largest));
+        System.out.print("The " + n + "th largest is: " + theNth);
     }
 
     //return the largest value in an array
-    private static int findLargest(int[] a) {
-        int max = a[0];
-        for(int i = 1; i < a.length; i++) {
-            if(a[i] > max) {
-                max = a[i];
-            }
-        }
-        return max;
-    }
+    private static int findNth(int[] a, int[] b, int aLen, int bLen, int n) {
 
-    //return the largest of two values
-    private static int largestVal(int a, int b) {
-        return Math.max(a, b);
+        int [] sorted = new int[aLen + bLen];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while(i<aLen && j<bLen){
+            if (a[i] < b[j])
+                sorted[k++] = a[i++];
+            else
+                sorted[k++] = b[j++];
+        }
+
+        while (i < aLen)
+            sorted[k++] = a[i++];
+        while (j < bLen)
+            sorted[k++] = b[j++];
+
+        return sorted[n - 1];
     }
 }
